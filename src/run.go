6ae781e3aea0645ad69c6e51b4fcf4351eb2b9b5
esllo <master@esllo.com>
2022-04-03 01:24:27 +0900

    add article 5

--- src/run.go
+++ src/run.go
@@ -71,9 +71,74 @@ func article4_DataType() {
 	fmt.Printf("bytes : %s\nstr   : %s", bytes, str2)
 }
 
+func article5_Conditional() {
+	// Statements
+
+	// If
+	num := 5
+	if num == 3 {
+		println("num is 3")
+	} else if num == 5 {
+		println("num is 5")
+	} else {
+		println("other")
+	}
+
+	// Optional
+	if PI := 3.14; PI == 3.14 {
+		println("PI = 3.14")
+	}
+
+	// Switch (always break)
+	category := 1
+	switch category {
+	case 1:
+		println("category 1")
+	case 2:
+		println("category 2")
+	default:
+		println("category 0")
+	}
+
+	// With Optional, Expression and fallthrough
+	switch queue := 2; queue - 1 {
+	case 1:
+		println("queue 1")
+		fallthrough // check next case
+	case 2, 3: // multiple cases 2, 3
+		println("queue 2")
+		fallthrough // check next case
+	default:
+		println("queue end")
+	}
+
+	// Empty Switch => alternative (if... else if...else if...)
+	name := "Park"
+	switch {
+	case name == "Kim":
+		println("Kim")
+	case name == "Park":
+		println("park")
+	case name == "Lee":
+		println("Lee")
+	}
+
+	// Type Check Switch
+	var flag interface{} = false
+	switch flag.(type) {
+	case int:
+		println("flag is int")
+	case string:
+		println("flag is string")
+	case bool:
+		println("flag is bool")
+	}
+}
+
 func main() {
 	article(1, article1_Print)
 	article(2, article2_FormattedPrint)
 	article(3, article3_Variable)
 	article(4, article4_DataType)
+	article(5, article5_Conditional)
 }
