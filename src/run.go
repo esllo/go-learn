5096437156d37796287935282701eafdca7528bd
esllo <master@esllo.com>
2022-04-03 01:09:37 +0900

    add article 4

--- src/run.go
+++ src/run.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func article(i int, f func()) {
 	println("================")
@@ -43,8 +45,35 @@ func article3_Variable() {
 	fmt.Printf("NC %f, SC %s\n", numConstant, myaConstant)
 }
 
+func article4_DataType() {
+	// Types
+	/*
+		int int8 int16 int32 int64
+		uint uint8 ... uintptr
+		float32 float64 complex64 complex128
+		bool
+		string(immutable)
+			`` => Raw String Literal(Multiline) > \n => \n
+			"" => Interpreted String Literal > \n => New Line
+		byte(8)
+		rune(32) Unicode Code point ex) 00000430 = A
+	*/
+	// Cast
+	var numInt int = 100
+	var numUInt = uint(numInt)
+	var numFloat = float32(numUInt)
+	fmt.Printf("%d, %d, %f\n", numInt, numUInt, numFloat)
+
+	// ":=" = Short Assignment Statement (define and assignment)
+	str := "Hello world"
+	bytes := []byte(str)
+	str2 := string(bytes)
+	fmt.Printf("bytes : %s\nstr   : %s", bytes, str2)
+}
+
 func main() {
 	article(1, article1_Print)
 	article(2, article2_FormattedPrint)
 	article(3, article3_Variable)
+	article(4, article4_DataType)
 }
